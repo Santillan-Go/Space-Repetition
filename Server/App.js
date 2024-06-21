28d44ce58279216441443ca054e4ba698838814a
Santillan-C <santillango10405@gmail.com>
2024-06-20 20:38:16 -0700

    username unique

--- Server/App.js
+++ Server/App.js
@@ -1,124 +1,118 @@
 import express from "express";
 import cors from "cors";
 import { User_DB_M } from "./MODEL/Users_DB_M.js";
-import  {PORT } from "./config.js";
-
+import { PORT } from "./config.js";
 
 //PROBAR API EN TODOS LOS CASOS
 
-
-
-
-const app=express();
+const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 //GET ALL USER
-app.get("/",async(req,res)=>{
-    
-    const result=await User_DB_M.getAllUser();
-    res.json(result); 
-        // res.json({msg:"GOOD"});
-    })
+app.get("/", async (req, res) => {
+  const result = await User_DB_M.getAllUser();
+  res.json(result);
+  // res.json({msg:"GOOD"});
+});
 
 //GET ONE USER
-app.get("/one-user/:id",async(req,res)=>{
-let id=parseInt(req.params.id);
-const body=req.body;
-    if(id){
-const result=await User_DB_M.getOneUserById({id});
+app.get("/one-user/:id", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const body = req.body;
+  if (id) {
+    const result = await User_DB_M.getOneUserById({ id });
 
-if(result){
+    if (result) {
+      res.json(result);
+    } else {
+      res
+        .status(404)
+        .json({ error: true, Message: "This acount doesn't exist" });
+    }
+  } else {
+    const result = await User_DB_M.getOneUser({ body });
+    if (result) {
+      res.json(result);
+    } else {
+      res
+        .status(404)
+        .json({ error: true, Message: "This acount doesn't exist" });
+    }
+  }
+  //const id=parseInt(req.params.id);
+
+  // res.json({msg:"GOOD"});
+});
+
+app.post("/one-user/", async (req, res) => {
+  const body = req.body;
+  const result = await User_DB_M.getOneUser({ body });
+  if (result) {
     res.json(result);
-    }else{
-res.status(404).json({error:true,Message:"This acount doesn't exist"})
-    }
-
-    }else{
- const result=await User_DB_M.getOneUser({body});
-    if(result){
-        res.json(result);
-        }else{
-    res.status(404).json({error:true,Message:"This acount doesn't exist"})
-        }
-    }
-    //const id=parseInt(req.params.id);
-
-   
-    // res.json({msg:"GOOD"});
-})
-
-app.post("/one-user/",async(req,res)=>{
-    const body=req.body;
-    const result=await User_DB_M.getOneUser({body});
-    if(result){
-        res.json(result);
-        }else{ 
-    res.status(404).json({error:true,Message:"This acount doesn't exist"})
-        }
-})
+  } else {
+    res.status(404).json({ error: true, Message: "This acount doesn't exist" });
+  }
+});
 
 ///PARA CREAR USER
-app.post("/users",async(req,res)=>{
-    let input=req.body;
-    input={...input,decks:[]} 
-    if(!input._id){
-input._id=Date.now();
+app.post("/users", async (req, res) => {
+  let input = req.body;
+  const username = req.body.username;
+  input = { ...input, decks: [] };
+  if (!input._id) {
+    input._id = Date.now();
+  }
+  try {
+    const userFound = await User_DB_M.getUserByName({ username });
+    if (userFound)
+      return res.status(400).json({ message: "User Already exist" });
+    const result = await User_DB_M.createOneUser({ input });
+    if (!result.error) {
+      res.json(result);
+    } else {
+      res.status(404).json({ message: "Error to create the user" });
     }
-    
-    const result=await User_DB_M.createOneUser({input});
-    if(!result.error){
-    res.json(result);
-    }else{
-res.status(404).json(result)
-    }
-
-    })
+  } catch (error) {
+    res.status(500).json({ message: "Error Server Internal" });
+  }
+});
 
 //PARA CREAR DECKS
 //ID=> USER_ID
-app.post("/decks/:id",async(req,res)=>{
-    let deck=req.body;
-    let id=parseInt(req.params.id);
-  
-    const result=await User_DB_M.createOneDeck({deck,id}); 
-    res.json(result);
-        // res.json({msg:"GOOD"});
-    })
-  
+app.post("/decks/:id", async (req, res) => {
+  let deck = req.body;
+  let id = parseInt(req.params.id);
+
+  const result = await User_DB_M.createOneDeck({ deck, id });
+  res.json(result);
+  // res.json({msg:"GOOD"});
+});
+
 // PARA CREAR CARTAS
-app.post("/decks/cards/:user/:deck",async(req,res)=>{
-    //id  deck
-    let {user,deck}=req.params;
-    let card=req.body;
-    // input={...input,decks:[]}
-    const result=await User_DB_M.createOneCard({card,user,deck});
-    res.json(result);
-        // res.json({msg:"GOOD"});
-    })
+app.post("/decks/cards/:user/:deck", async (req, res) => {
+  //id  deck
+  let { user, deck } = req.params;
+  let card = req.body;
+  // input={...input,decks:[]}
+  const result = await User_DB_M.createOneCard({ card, user, deck });
+  res.json(result);
+  // res.json({msg:"GOOD"});
+});
 
 // PARA ACTUALIZAR CARTAS
 
-app.patch("/decks/cards/:user/:deck/:card",async(req,res)=>{
-    let {user,card,deck}=req.params;
-    const input=req.body;
-    // input={...input,decks:[]}
-    const result=await User_DB_M.updateCardOne({input,user,card,deck});
-    res.json(result);
-        // res.json({msg:"GOOD"});
-    })
-
-
-app.listen(PORT,()=>{
-    console.log(`server running on http://localhost:`+PORT);
+app.patch("/decks/cards/:user/:deck/:card", async (req, res) => {
+  let { user, card, deck } = req.params;
+  const input = req.body;
+  // input={...input,decks:[]}
+  const result = await User_DB_M.updateCardOne({ input, user, card, deck });
+  res.json(result);
+  // res.json({msg:"GOOD"});
 });
 
-// {
-
-//     "username": "Santy",
-//      "password": "San129?"
-     
-//  }
-//http://localhost:4000/decks/cards/9/1
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:` + PORT);
+});
--- Server/MODEL/Users_DB_M.js
+++ Server/MODEL/Users_DB_M.js
@@ -1,154 +1,118 @@
 import { connectDB, desconnectDB } from "./ConectDB.js";
 import db from "./ConectOneDB.js";
 
+const db_name = "users_db",
+  collectionName = "users";
 
-const db_name="users_db",
- collectionName="users";
-
- //CREATE USER
- // CREATE DESK
- //CREAE CARD
- // Updata card
+//CREATE USER
+// CREATE DESK
+//CREAE CARD
+// Updata card
 //GOOD
-const  getAllUser= async()=>{
-
-    
-    const collection= db.collection(collectionName);
-    const result= await collection.find({}).toArray();
-    return result;
-    
-   
-    }
-
-
-
-//GOOD
-
-const getOneUser= async({body})=>{
-
-    let {username,password}=body;
-   
-    const collection= db.collection(collectionName);
-    const result= await collection.findOne({username,password});
-    return result;
-    
-   
-    }
-
-    const getOneUserById= async({id})=>{
-
-        //let {username,password}=body;
-      
-        
-        const collection= db.collection(collectionName);
-        const result= await collection.findOne({_id:id});
-        return result;
-        
-      
-        }
-
-//GOOD
-
-const createOneUser= async({input})=>{
-  
-
-    
-    const collection= db.collection(collectionName);
-    
-    const userExist=await collection.findOne({username:input.username});
-
-    if(userExist){
-     
-return {mensaje:"Este usuario ya exite",error:true}
-    }else{
-  
-         const result= await collection.insertOne(input);
-    return result;
-    
-    }
-   
-  
-    }
-
-
-//GOOD
-const createOneDeck=async({deck,id})=>{
-    
-    
-    const collection= db.collection(collectionName);
-    const result= await collection.updateOne({_id:id},{$push:{decks:deck}})
-    return result;
-    
-    
-}
-
-//GOOD
-const createOneCard=async({deck,user,card})=>{
-
-    
-    const collection= db.collection(collectionName);
-    const result= await collection.updateOne({_id:parseInt(user), "decks.id":parseInt(deck)},{$push:{"decks.$.cards":card}})
-    return result;
-    
-    
-}
-
-//===
-const updateCardOne=async({user,deck,card,input})=>{
-    let client;
-    let {nextTime,lastReviewDate}=input
-    lastReviewDate=lastReviewDate||"";
-
-    const collection=db.collection(collectionName);
-   const result=await collection.updateOne(
-        { _id: parseInt(user), "decks.id": parseInt(deck), "decks.cards.id": parseInt(card) }, // Filtro para encontrar la carta específica en el mazo del usuario
-        { $set: { 
-            "decks.$[deckElem].cards.$[cardElem].nextTime": nextTime ,
-            "decks.$[deckElem].cards.$[cardElem].lastReviewDate": lastReviewDate
-    } }, // Actualizar la propiedad nextTime de la carta
-        { arrayFilters: [{ "deckElem.id": parseInt(deck) }, { "cardElem.id": parseInt(card) }] } // Filtros para los elementos del array
-      )
-
- 
-    return result;
-    
-
-}
-
-
-
-
-export const User_DB_M={
-    getOneUser,
-    createOneUser
-    ,createOneCard,
-   createOneDeck,
-   updateCardOne,
-   getAllUser,
-   getOneUserById
+const getAllUser = async () => {
+  const collection = db.collection(collectionName);
+  const result = await collection.find({}).toArray();
+  return result;
 };
 
+const getUserByName = async ({ username }) => {
+  const collection = db.collection(collectionName);
 
+  const result = await collection.findOne({ username });
 
-// const  getAllUser= async()=>{
-// let client;
-// try{
-// client=await connectDB();
+  return result;
+};
 
-// const db= client.db(db_name);
+//GOOD
 
-// const collection= db.collection(collectionName);
-// const result= await collection.find({}).toArray();
-// return result;
+const getOneUser = async ({ body }) => {
+  let { username, password } = body;
 
-// }
-// catch(err){
-// throw err
-// }
-// finally{
-//     if(client){
-// await desconnectDB(client)
+  const collection = db.collection(collectionName);
+  const result = await collection.findOne({ username, password });
+  return result;
+};
 
-//     }
-// }
-// }
+const getOneUserById = async ({ id }) => {
+  //let {username,password}=body;
+
+  const collection = db.collection(collectionName);
+  const result = await collection.findOne({ _id: id });
+  return result;
+};
+
+//GOOD
+
+const createOneUser = async ({ input }) => {
+  const collection = db.collection(collectionName);
+
+  const userExist = await collection.findOne({ username: input.username });
+
+  if (userExist) {
+    return { mensaje: "Este usuario ya exite", error: true };
+  } else {
+    const result = await collection.insertOne(input);
+    return result;
+  }
+};
+
+//GOOD
+const createOneDeck = async ({ deck, id }) => {
+  const collection = db.collection(collectionName);
+  const result = await collection.updateOne(
+    { _id: id },
+    { $push: { decks: deck } }
+  );
+  return result;
+};
+
+//GOOD
+const createOneCard = async ({ deck, user, card }) => {
+  const collection = db.collection(collectionName);
+  const result = await collection.updateOne(
+    { _id: parseInt(user), "decks.id": parseInt(deck) },
+    { $push: { "decks.$.cards": card } }
+  );
+  return result;
+};
+
+//===
+const updateCardOne = async ({ user, deck, card, input }) => {
+  let client;
+  let { nextTime, lastReviewDate } = input;
+  lastReviewDate = lastReviewDate || "";
+
+  const collection = db.collection(collectionName);
+  const result = await collection.updateOne(
+    {
+      _id: parseInt(user),
+      "decks.id": parseInt(deck),
+      "decks.cards.id": parseInt(card),
+    }, // Filtro para encontrar la carta específica en el mazo del usuario
+    {
+      $set: {
+        "decks.$[deckElem].cards.$[cardElem].nextTime": nextTime,
+        "decks.$[deckElem].cards.$[cardElem].lastReviewDate": lastReviewDate,
+      },
+    }, // Actualizar la propiedad nextTime de la carta
+    {
+      arrayFilters: [
+        { "deckElem.id": parseInt(deck) },
+        { "cardElem.id": parseInt(card) },
+      ],
+    } // Filtros para los elementos del array
+  );
+
+  return result;
+};
+
+export const User_DB_M = {
+  getOneUser,
+  createOneUser,
+  createOneCard,
+  createOneDeck,
+  updateCardOne,
+  getAllUser,
+  getOneUserById,
+  getUserByName,
+};
